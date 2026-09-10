4454c118da502e7a83470f68bd74eebcbb044931
yaqob-M-almateri <yaqob.m.almateri@gmail.com>
2026-09-10 07:07:49 +0300

    Update DB Connection

--- dbConnection.php
+++ dbConnection.php
@@ -1,8 +1,8 @@
 <?php
-$databaseHost = 'localhost';
-$databaseName = 'test';
-$databaseUsername = 'root';
-$databasePassword = 'root';
+$databaseHost = 'sql202.infinityfree.com';
+$databaseName = 'if0_42878651_crud_db';
+$databaseUsername = 'if0_42878651';
+$databasePassword = 'ynyn12yn';
 
 // Open a new connection to the MySQL server
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
