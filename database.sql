212f0fbeed8f6604d1081b5a98285c729253727a
al-materi <yaqob1it@gmail.com>
2026-09-16 08:41:38 +0300

    add user to DataBase

--- database.sql
+++ database.sql
@@ -1,7 +1,11 @@
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(100) NOT NULL,
-  `age` int(3) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  PRIMARY KEY  (`id`)
-);
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `age` INT(3) NOT NULL,
+  `email` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `users` (`name`, `age`, `email`) VALUES
+('يعقوب الماطري', 22, 'yaqob-m-almateri@gmail.com'),
+('al-materi', 23, 'yaqob1it@gmail.com');
--- dbConnection.php
+++ dbConnection.php
@@ -1,12 +1,12 @@
-<!-- <?php
-$databaseHost = 'sql202.infinityfree.com';
-$databaseName = 'if0_42878651_crud_db';
-$databaseUsername = 'if0_42878651';
-$databasePassword = 'ynyn12yn';
-
-// Open a new connection to the MySQL server
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-?> -->
+//<!-- <?php
+//$databaseHost = 'sql202.infinityfree.com';
+//$databaseName = 'if0_42878651_crud_db';
+//$databaseUsername = 'if0_42878651';
+//$databasePassword = 'ynyn12yn';
+//
+//// Open a new connection to the MySQL server
+//$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+//?> -->
 
 <?php
 /**
